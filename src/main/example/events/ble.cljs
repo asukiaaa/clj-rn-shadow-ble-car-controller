c0b012fd6ec8e8a59abdf2682a4ced544548b845
Asuki Kono <asukiaaa@gmail.com>
2024-07-23 15:33:05 +0900

    Create event not to call subscribe on outside of reactive context

--- src/main/example/events/ble.cljs
+++ src/main/example/events/ble.cljs
@@ -1,24 +1,22 @@
 (ns example.events.ble
   (:require ["moment" :as moment]
             [example.devices.ble :as ble]
-            [re-frame.core :refer [subscribe dispatch reg-event-fx]]))
+            [re-frame.core :refer [dispatch reg-event-db]]))
 
 (def service-id "00ff")
 (def characteristic-id "ff01")
 
-(defn- ble-send [{:keys [l r]}]
-  (let [current-device (subscribe [:get-current-device])
-        data (concat [(.charCodeAt "m" 0)]
+(defn- ble-send [{:keys [l r]} id-device]
+  (let [data (concat [(.charCodeAt "m" 0)]
                      (map #(if (nil? %) 0 %) [l r]))]
-    (ble/write (:id @current-device) service-id characteristic-id data)))
+    (ble/write id-device service-id characteristic-id data)))
 
-(defn- ble-send-direction [{:keys [degree speed]}]
-  (let [current-device (subscribe [:get-current-device])
-        degree (int degree)
+(defn- ble-send-direction [{:keys [degree speed]} id-device]
+  (let [degree (int degree)
         plus-degree (if (> degree 0) degree 0)
         minus-degree (if (< degree 0) (- degree) 0)
         data [(.charCodeAt "d" 0) plus-degree minus-degree (int speed)]]
-    (ble/write (:id @current-device) service-id characteristic-id data)))
+    (ble/write id-device service-id characteristic-id data)))
 
 (defn- same-speed? [speed1 speed2]
   (and (= (:l speed1) (:r speed2))
@@ -31,21 +29,35 @@
          :when (and (not= speed 128) (not= speed 127))]
      key)))
 
+(reg-event-db
+ :ble-send-speed
+ (fn [db _]
+   (let [speed (:speed db)
+         sent-speed (:sent-speed db)
+         id-current-device (-> (:current-device db) :id)
+         now-50ms (.subtract (moment) 50 "ms")]
+     (if (or (not (same-speed? speed sent-speed))
+             (and (not (stop-speed? speed))
+                  (.isAfter now-50ms (moment (:sent_at sent-speed)))))
+       (do
+         (ble-send speed id-current-device)
+         (assoc db :sent-speed (assoc speed :sent_at (js/Date.))))
+       db))))
+
 (defn send-speed []
-  (let [speed (subscribe [:speed])
-        sent-speed (subscribe [:sent-speed])
-        now-50ms (.subtract (moment) 50 "ms")]
-    #_(.log js/console "send")
-    (when (or (not (same-speed? @speed @sent-speed))
-              (and (not (stop-speed? @speed))
-                   (.isAfter now-50ms (moment (:sent_at @sent-speed)))))
-      #_(println "send" sent-speed)
-      (ble-send @speed)
-      (dispatch [:set-sent-speed (assoc @speed :sent_at (js/Date.))]))))
+  (dispatch [:ble-send-speed]))
+
+(reg-event-db
+ :ble-send-direction
+ (fn [db _]
+   (let [direction-speed (:direction-speed db)
+         sent-direction-speed (:sent-direction-speed :db)
+         id-current-device (-> (:current-device db) :id)]
+     (if (and (= 0 (:speed sent-direction-speed)) (= 0 (:speed direction-speed)))
+       db
+       (do
+         (ble-send-direction direction-speed id-current-device)
+         (assoc db :sent-direction-speed direction-speed))))))
 
 (defn send-direction []
-  (let [direction-speed (subscribe [:direction-speed])
-        sent-direction-speed (subscribe [:sent-direction-speed])]
-    (when-not (and (= 0 (:speed @sent-direction-speed)) (= 0 (:speed @direction-speed)))
-      (ble-send-direction @direction-speed)
-      (dispatch [:set-sent-direction-speed @direction-speed]))))
+  (dispatch :ble-send-direction))
